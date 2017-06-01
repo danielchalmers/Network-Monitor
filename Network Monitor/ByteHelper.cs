548e0445ae5721384c545943a02ad32a57fc2de7
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-06-01 19:45:39 -0400

    Format code

--- Network Monitor/ByteHelper.cs
+++ Network Monitor/ByteHelper.cs
@@ -8,15 +8,15 @@ namespace Network_Monitor
         public static string BytesToString(long byteCount)
         {
             // Longs run out around EB.
-            string[] suffix = {"B", "KB", "MB", "GB", "TB", "PB", "EB"};
+            string[] suffix = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
             if (byteCount == 0)
             {
                 return "0" + suffix[0];
             }
             var bytes = Math.Abs(byteCount);
             var place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
-            var num = Math.Round(bytes/Math.Pow(1024, place), 1);
-            return Math.Sign(byteCount)*num + suffix[place];
+            var num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return Math.Sign(byteCount) * num + suffix[place];
         }
     }
 }
