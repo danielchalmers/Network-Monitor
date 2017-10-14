53123e21423d5ab5bae687aa7431c245e7bb1f51
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-10-14 14:00:20 -0400

    Use assembly version for file version

--- Network Monitor/Properties/AssemblyInfo.cs
+++ Network Monitor/Properties/AssemblyInfo.cs
@@ -53,5 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: Guid("FB1814E0-AA8C-47EF-96A9-5F936CB0DF3F")]
