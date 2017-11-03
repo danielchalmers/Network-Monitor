44527abefeae230205e6c5ec370a2222065cf47e
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-11-03 14:50:05 -0400

    2.1.0

--- Network Monitor/Properties/AssemblyInfo.cs
+++ Network Monitor/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0")]
 [assembly: Guid("FB1814E0-AA8C-47EF-96A9-5F936CB0DF3F")]
